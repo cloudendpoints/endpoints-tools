2408a763a5bf2c06cb16145639e8b69a85be3ecb
Jun Mukai <mukai@google.com>
2017-04-07 15:30:43 -0700

    Set 443 for the default port of https backend.
    Fixes #7

--- start_esp/start_esp.py
+++ start_esp/start_esp.py
@@ -228,7 +228,10 @@ def make_ingress(service_config, args):
         backends = [args.backend[len(HTTP_PREFIX):]]
     elif args.backend.startswith(HTTPS_PREFIX):
         proto = "https"
-        backends = [args.backend[len(HTTPS_PREFIX):]]
+        backend = args.backend[len(HTTPS_PREFIX):]
+        if backend.find(':') < 0:
+            backend = backend + ':443'
+        backends = [backend]
     else:
         proto = "http"
         backends = [args.backend]
