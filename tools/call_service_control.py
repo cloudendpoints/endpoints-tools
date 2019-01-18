cd610cfa7470d3c6c14088e2107520ddfab5ca2e
Wayne Zhang <qiwzhang@gmail.com>
2019-01-18 11:38:09 -0800

    add license title and rename it

--- tools/call_service_control.py
+++ tools/call_service_control.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python -u
+#
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import argparse
 import urllib2
@@ -7,10 +21,10 @@ import time
 import uuid
 
 """
-This script will repeatly call Google service control service on every second
+This script will repeatedly call Google service control service on every second
 to detect big latency.  Its usage
 
-./call.py --api_key=$API_KEY  --sevice=$SERVICE
+./call_service_control.py --api_key=$API_KEY  --sevice=$SERVICE
 
 SERVICE: an endpoint service for service control Check and Report.
 API_KEY: an api key of the project.
